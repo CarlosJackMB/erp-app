e4871b5c40cae0ba193a03373b273d1dae2f915f
CarlosJackMB <martinbolanoscarlos@gmail.com>
2025-07-14 11:52:03 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from app.modules.laboral.routes import laboral_bp
 
 def create_app(config_name="development"):
     app = Flask(__name__)
-    app.config.from_file(f"config/{config_name}.yaml", load=dict)
+    app.config.from_file(f"../config/{config_name}.yaml", load=dict)
     app.register_blueprint(main)
     app.register_blueprint(personal_bp, url_prefix="/personal")
     app.register_blueprint(laboral_bp, url_prefix="/laboral")
